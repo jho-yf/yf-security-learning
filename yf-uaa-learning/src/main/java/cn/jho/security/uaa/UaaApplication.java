5215383187070ba9c0c543bb584a5454098a3746
jho-yf <xu-jihong@qq.com>
2022-05-01 00:07:39 +0800

    feat: 初始化uaa用户认证和授权服务器

---
+++ yf-uaa-learning/src/main/java/cn/jho/security/uaa/UaaApplication.java
@@ -0,0 +1,17 @@
+package cn.jho.security.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JHO xu-jihong@qq.com
+ * @date 2022-05-01 0:03
+ */
+@SpringBootApplication
+public class UaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UaaApplication.class, args);
+    }
+
+}
